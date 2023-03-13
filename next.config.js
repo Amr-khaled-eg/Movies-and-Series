d293bd85b55003295277324329f37d3ad06369fd
amr <amrkhaled.m27@gmail.com>
2023-03-13 22:00:26 +0200

    add styles to all the components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/original/**",
+        port: "",
+      },
+    ],
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navigation from '@/components/navigation/navigation.component'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+  <Navigation/>
+  <Component {...pageProps} />
+  </>);
 }
