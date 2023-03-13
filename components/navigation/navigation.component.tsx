aeb6d69a544099779b3e3892d1f21dee71e95cee
amr <amrkhaled.m27@gmail.com>
2023-03-13 21:59:01 +0200

    create the navigation bar copmonent

---
+++ components/navigation/navigation.component.tsx
@@ -0,0 +1,40 @@
+import styles from "@/styles/components/navigation/navigation.module.css";
+import Logo from "@/public/Logo.svg";
+import Image from "next/image";
+import Search from "../search/search.component";
+import NavLinks from "./nav-links/nav-links.component";
+import burger from "@/public/burger.svg";
+import SearchIcon from "@/public/search.svg";
+import { useState } from "react";
+const Navigation = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <nav className={styles.navContainer}>
+      <Image src={Logo} alt="logo" />
+      <Search icon={true} show={isSearchOpen} toggleShow={toggleSearch} />
+      <div className={styles.navIconsContainer}>
+        <Image
+          src={SearchIcon}
+          alt="search icon"
+          className={styles.navSearchIcon}
+          onClick={toggleSearch}
+        />
+        <Image
+          src={burger}
+          alt="burger icon"
+          className={styles.burger}
+          onClick={toggleMenu}
+        />
+      </div>
+      <NavLinks show={isMenuOpen} toggleShow={toggleMenu} />
+    </nav>
+  );
+};
+export default Navigation;
