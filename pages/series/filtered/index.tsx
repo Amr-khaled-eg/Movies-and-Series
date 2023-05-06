0b3f2ba5bd2fd21014314d8acdb1dafc2335346d
amr <amrkhaled.m27@gmail.com>
2023-05-06 21:55:04 +0300

    create series route

---
+++ pages/series/filtered/index.tsx
@@ -0,0 +1,33 @@
+import { FetchMyAPI } from "@/utils/fetch/fetch-data.utils";
+import { GetServerSidePropsContext } from "next";
+import { Item } from "..";
+import Inventory from "@/components/inventory/inventory.component";
+import Filter from "@/components/filter/fiilter.comoponent";
+import { useRouter } from "next/router";
+import { stringify } from "querystring";
+type FlteredItemsProps = {
+  initialItems: Item[];
+};
+const FetchMore = async (page: number, query: string): Promise<Item[]> => {
+  const data = await FetchMyAPI(`/series?page=${page}&${query}`);
+  return data.items;
+};
+export default function FilteredMovies({ initialItems }: FlteredItemsProps) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Filter />
+      <Inventory FetchMore={FetchMore} initialItems={initialItems} />
+    </>
+  );
+}
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
+  const initialItems = await FetchMore(1, stringify(query));
+
+  return {
+    props: {
+      initialItems: initialItems,
+    },
+  };
+}
---
+++ pages/series/index.tsx
@@ -0,0 +1,48 @@
+import CardsSlider from "@/components/cardsSlider/cards-slider.component";
+import { FetchMyAPI } from "@/utils/fetch/fetch-data.utils";
+import Trending from "@/public/trending.svg";
+import TopRated from "@/public/top-rated.svg";
+import Popular from "@/public/popular.svg";
+import { CardData } from "@/components/card/card.component";
+import { Categories } from "@/components/filter/fiilter.comoponent";
+import MainSlider from "@/components/main-slider/main-slider.component";
+import styles from "@/styles/home.module.css";
+export type Item = {
+  [key: string]: any;
+  overview: string;
+} & CardData;
+
+type MoviesProps = {
+  topRated: [Item];
+  popular: [Item];
+  trending: [Item];
+};
+
+export default function Seires({ topRated, trending, popular }: MoviesProps) {
+  return (
+    <section className={styles.mainSection}>
+      <MainSlider items={[topRated[0], topRated[1], trending[0], popular[0]]} />
+      <CardsSlider header="Top-Rated" cards={topRated} icon={TopRated} />
+      <CardsSlider header="Trending" cards={popular} icon={Trending} />
+      <CardsSlider header="Popular" cards={trending} icon={Popular} />
+    </section>
+  );
+}
+export async function getServerSideProps() {
+  const topRatedMovies = await FetchMyAPI(
+    `/series?category=${Categories.TOP_RATED}`
+  );
+  const trendingMovies = await FetchMyAPI(
+    `/series?category=${Categories.TRENDING}`
+  );
+  const popularMovies = await FetchMyAPI(
+    `/series?category=${Categories.TOP_RATED}`
+  );
+  return {
+    props: {
+      topRated: topRatedMovies.items,
+      trending: trendingMovies.items,
+      popular: popularMovies.items,
+    },
+  };
+}
