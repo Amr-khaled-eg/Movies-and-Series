e84044f58092338f89395187fe4490cdc121bb8b
amr <amrkhaled.m27@gmail.com>
2023-05-06 21:54:41 +0300

    add resourse files

---
+++ public/countries.js
@@ -0,0 +1,321 @@
+export const countries = {
+  SA: {
+    languageCode: "ar",
+    name: "Saudi Arabia",
+  },
+  EG: {
+    languageCode: "ar",
+    name: "Egypt",
+  },
+  AE: {
+    languageCode: "ar",
+    name: "United Arab Emirates",
+  },
+  IQ: {
+    languageCode: "ar",
+    name: "Iraq",
+  },
+  MA: {
+    languageCode: "ar",
+    name: "Morocco",
+  },
+
+  US: {
+    languageCode: "en",
+    name: "United States",
+  },
+  CN: {
+    languageCode: "zh",
+    name: "China",
+  },
+  JP: {
+    languageCode: "ja",
+    name: "Japan",
+  },
+  KR: {
+    languageCode: "ko",
+    name: "South Korea",
+  },
+  GB: {
+    languageCode: "en",
+    name: "United Kingdom",
+  },
+  FR: {
+    languageCode: "fr",
+    name: "France",
+  },
+  DE: {
+    languageCode: "de",
+    name: "Germany",
+  },
+  ES: {
+    languageCode: "es",
+    name: "Spain",
+  },
+  CA: {
+    languageCode: "en",
+    name: "Canada",
+  },
+  AU: {
+    languageCode: "en",
+    name: "Australia",
+  },
+};
+export const certifications = {
+  US: "R",
+  JP: "G",
+  KR: "15",
+  GB: "15",
+  FR: "TP",
+  DE: "12",
+  ES: "16",
+  CA: "18A",
+};
+
+// export const certifications = {
+//   SA: undefined,
+//   EG: undefined,
+//   AE: undefined,
+//   IQ: undefined,
+//   MA: undefined,
+//   US: [
+//     {
+//       certification: 'R',
+//       meaning: 'Under 17 requires accompanying parent or adult guardian 21 or older. The parent/guardian is required to stay with the child under 17 through the entire movie, even if the parent gives the child/teenager permission to see the film alone. These films may contain strong profanity, graphic sexuality, nudity, strong violence, horror, gore, and strong drug use. A movie rated R for profanity often has more severe or frequent language than the PG-13 rating would permit. An R-rated movie may have more blood, gore, drug use, nudity, or graphic sexuality than a PG-13 movie would admit.',
+//       order: 4
+//     },
+//     {
+//       certification: 'PG',
+//       meaning: 'Some material may not be suitable for children under 10. These films may contain some mild language, crude/suggestive humor, scary moments and/or violence. No drug content is present. There are a few exceptions to this rule. A few racial insults may also be heard.',
+//       order: 2
+//     },
+//     {
+//       certification: 'NC-17',
+//       meaning: 'These films contain excessive graphic violence, intense or explicit sex, depraved, abhorrent behavior, explicit drug abuse, strong language, explicit nudity, or any other elements which, at present, most parents would consider too strong and therefore off-limits for viewing by their children and teens. NC-17 does not necessarily mean obscene or pornographic in the oft-accepted or legal meaning of those words.',
+//       order: 5
+//     },
+//     {
+//       certification: 'G',
+//       meaning: 'All ages admitted. There is no content that would be objectionable to most parents. This is one of only two ratings dating back to 1968 that still exists today.',
+//       order: 1
+//     },
+//     {
+//       certification: 'NR',
+//       meaning: 'No rating information.',
+//       order: 0
+//     },
+//     {
+//       certification: 'PG-13',
+//       meaning: 'Some material may be inappropriate for children under 13. Films given this rating may contain sexual content, brief or partial nudity, some strong language and innuendo, humor, mature themes, political themes, terror and/or intense action violence. However, bloodshed is rarely present. This is the minimum rating at which drug content is present.',
+//       order: 3
+//     }
+//   ],
+//   CN: undefined,
+//   JP: [
+//     {
+//       certification: 'G',
+//       meaning: 'General, suitable for all ages.',
+//       order: 1
+//     },
+//     {
+//       certification: 'PG12',
+//       meaning: 'Parental guidance requested for young people under 12 years.',
+//       order: 2
+//     },
+//     {
+//       certification: 'R15+',
+//       meaning: 'No one under 15 admitted.',
+//       order: 3
+//     },
+//     {
+//       certification: 'R18+',
+//       meaning: 'No one under 18 admitted.',
+//       order: 4
+//     }
+//   ],
+//   KR: [
+//     {
+//       certification: 'All',
+//       meaning: 'Film suitable for all ages.',
+//       order: 0
+//     },
+//     {
+//       certification: '12',
+//       meaning: 'Film intended for audiences 12 and over. Underage audiences accompanied by a parent or guardian are allowed.',
+//       order: 1
+//     },
+//     {
+//       certification: '15',
+//       meaning: 'Film intended for audiences 15 and over. Underage audiences accompanied by a parent or guardian are allowed.',
+//       order: 2
+//     },
+//     {
+//       certification: '18',
+//       meaning: 'No one under 18 is allowed to watch this film.',
+//       order: 3
+//     },
+//     {
+//       certification: 'Restricted Screening',
+//       meaning: 'Film needs a certain restriction in screening or advertisement as it is considered a highly bad influence to universal human dignity, social value, good customs or national emotion due to excessive expression of nudity, violence, social behavior, etc. (technically not an age restriction but films with this rating may only be screened at "adults only" theatres, with the age of majority set at 19).',
+//       order: 4
+//     }
+//   ],
+//   GB: [
+//     {
+//       certification: '15',
+//       meaning: 'Only those over 15 years are admitted. Nobody younger than 15 can rent or buy a 15-rated VHS, DVD, Blu-ray Disc, UMD or game, or watch a film in the cinema with this rating. Films under this category can contain adult themes, hard drugs, frequent strong language and limited use of very strong language, strong violence and strong sex references, and nudity without graphic detail. Sexual activity may be portrayed but without any strong detail. Sexual violence may be shown if discreet and justified by context.',
+//       order: 5
+//     },
+//     {
+//       certification: 'R18',
+//       meaning: 'Can only be shown at licensed adult cinemas or sold at licensed sex shops, and only to adults, those aged 18 or over. Films under this category are always hard-core pornography, defined as material intended for sexual stimulation and containing clear images of real sexual activity, strong fetish material, explicit animated images, or sight of certain acts such as triple simultaneous penetration and snowballing. There remains a range of material that is often cut from the R18 rating: strong images of injury in BDSM or spanking works, urolagnia, scenes suggesting incest even if staged, references to underage sex or childhood sexual development and aggressive behaviour such as hair-pulling or spitting on a performer are not permitted. More cuts are demanded in this category than any other category.',
+//       order: 7
+//     },
+//     {
+//       certification: 'PG',
+//       meaning: 'All ages admitted, but certain scenes may be unsuitable for young children. May contain mild language and sex/drugs references. May contain moderate violence if justified by context (e.g. fantasy).',
+//       order: 2
+//     },
+//     {
+//       certification: '12A',
+//       meaning: 'Films under this category are considered to be unsuitable for very young people. Those aged under 12 years are only admitted if accompanied by an adult, aged at least 18 years, at all times during the motion picture. However, it is generally not recommended that children under 12 years should watch the film. Films under this category can contain mature themes, discrimination, soft drugs, moderate swear words, infrequent strong language and moderate violence, sex references and nudity. Sexual activity may be briefly and discreetly portrayed. Sexual violence may be implied or briefly indicated.',
+//       order: 3
+//     },
+//     {
+//       certification: 'U',
+//       meaning: 'All ages admitted, there is nothing unsuitable for children.',
+//       order: 1
+//     },
+//     {
+//       certification: '18',
+//       meaning: 'Only adults are admitted. Nobody younger than 18 can rent or buy an 18-rated VHS, DVD, Blu-ray Disc, UMD or game, or watch a film in the cinema with this rating. Films under this category do not have limitation on the bad language that is used. Hard drugs are generally allowed, and explicit sex references along with detailed sexual activity are also allowed. Scenes of strong real sex may be permitted if justified by the context. Very strong, gory, and/or sadistic violence is usually permitted. Strong sexual violence is permitted unless it is eroticised or excessively graphic.',
+//       order: 6
+//     },
+//     {
+//       certification: '12',
+//       meaning: 'Home media only since 2002. 12A-rated films are usually given a 12 certificate for the VHS/DVD version unless extra material has been added that requires a higher rating. Nobody younger than 12 can rent or buy a 12-rated VHS, DVD, Blu-ray Disc, UMD or game. The content guidelines are identical to those used for the 12A certificate.',
+//       order: 4
+//     }
+//   ],
+//   FR: [
+//     {
+//       certification: 'TP',
+//       meaning: 'Valid for all audiences.',
+//       order: 1
+//     },
+//     {
+//       certification: '12',
+//       meaning: 'Unsuitable for children younger than 12 or forbidden in cinemas for under 12.',
+//       order: 2
+//     },
+//     {
+//       certification: '16',
+//       meaning: 'Unsuitable for children younger than 16 or forbidden in cinemas for under 16.',
+//       order: 3
+//     },
+//     {
+//       certification: '18',
+//       meaning: 'Unsuitable for children younger than 18 or forbidden in cinemas for under 18.',
+//       order: 4
+//     }
+//   ],
+//   DE: [
+//     {
+//       certification: '12',
+//       meaning: 'Children 12 or older admitted, children between 6 and 11 only when accompanied by parent or a legal guardian.',
+//       order: 3
+//     },
+//     {
+//       certification: '18',
+//       meaning: 'No youth admitted, only adults.',
+//       order: 5
+//     },
+//     { certification: '0', meaning: 'No age restriction.', order: 1 },
+//     {
+//       certification: '6',
+//       meaning: 'No children younger than 6 years admitted.',
+//       order: 2
+//     },
+//     {
+//       certification: '16',
+//       meaning: 'Children 16 or older admitted, nobody under this age admitted.',
+//       order: 4
+//     }
+//   ],
+//   ES: [
+//     { certification: 'A', meaning: 'General admission.', order: 1 },
+//     { certification: 'Ai', meaning: 'General admission.', order: 2 },
+//     {
+//       certification: '7',
+//       meaning: 'Not recommended for audiences under 7.',
+//       order: 3
+//     },
+//     {
+//       certification: '7i',
+//       meaning: 'Not recommended for audiences under 7.',
+//       order: 4
+//     },
+//     {
+//       certification: '12',
+//       meaning: 'Not recommended for audiences under 12.',
+//       order: 5
+//     },
+//     {
+//       certification: '16',
+//       meaning: 'Not recommended for audiences under 16.',
+//       order: 5
+//     },
+//     {
+//       certification: '18',
+//       meaning: 'Not recommended for audiences under 18.',
+//       order: 7
+//     },
+//     {
+//       certification: 'X',
+//       meaning: 'Prohibited for audiences under 18.',
+//       order: 8
+//     }
+//   ],
+//   CA: [
+//     { certification: 'G', meaning: 'All ages.', order: 2 },
+//     {
+//       certification: 'PG',
+//       meaning: 'Parental guidance advised. There is no age restriction but some material may not be suitable for all children.',
+//       order: 3
+//     },
+//     {
+//       certification: '14A',
+//       meaning: 'Persons under 14 years of age must be accompanied by an adult.',
+//       order: 4
+//     },
+//     {
+//       certification: '18A',
+//       meaning: 'Persons under 18 years of age must be accompanied by an adult. In the Maritimes & Manitoba, children under the age of 14 are prohibited from viewing the film.',
+//       order: 5
+//     },
+//     {
+//       certification: 'R',
+//       meaning: 'Restricted to 18 years and over. No rental or purchase by those under 18. Content not suitable for minors. Video contains frequent use of: sexual activity; brutal/graphic violence; intense horror; and/or other disturbing content.',
+//       order: 6
+//     },
+//     {
+//       certification: 'A',
+//       order: 5
+//     },
+//     {
+//       certification: 'R 18+',
+//       meaning: 'Restricted to 18 years and over. Adults only. The content is high in impact.',
+//       order: 6
+//     },
+//     {
+//       certification: 'X 18+',
+//       meaning: 'Restricted to 18 years and over. Films with this rating have pornographic content. Films classified as X18+ are banned from being sold or rented in all Australian states and are only legally available in the Australian Capital Territory and the Northern Territory. However, importing X18+ material from the two territories to any of the Australian states is legal.The content is sexually explicit in impact.',
+//       order: 7
+//     },
+//     {
+//       certification: 'RC',
+//       meaning: 'Refused Classification. Banned from sale or hire in Australia; also generally applies to importation (if inspected by and suspicious to Customs). Private Internet viewing is unenforced and attempts to legally censor such online material has resulted in controversy. Films are rated RC if their content exceeds the guidelines. The content is very high in impact.',
+//       order: 8
+//     }
+//   ]
+// }
