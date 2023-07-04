ba17f7e04ca34c82d0161709061d700614d2c4c6
amr <amrkhaled.m27@gmail.com>
2023-07-04 14:30:06 +0300

    change to the URL instead of localhost

--- utils/fetch/fetch-data.utils.ts
+++ utils/fetch/fetch-data.utils.ts
@@ -1,5 +1,4 @@
 const URL = "https://movies-and-series-ecru.vercel.app/api";
-const another = "http://localhost:3000/api";
 export const Fetch = async (url: string, options?: RequestInit) => {
   try {
     const response = await fetch(url, {
@@ -16,4 +15,4 @@ export const Fetch = async (url: string, options?: RequestInit) => {
   }
 };
 export const FetchMyAPI = async (endpoint: string, options?: RequestInit) =>
-  await Fetch(another + endpoint, options);
+  await Fetch(URL + endpoint, options);
